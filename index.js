8e63340194e3a2b3e2b8110703c9e2707d55febf
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-07 16:52:14 -1000

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code here!
-const main = document.getElementById('main');
-main.remove(); 
+const main = document.getElementById("main");
+main.remove();
 
-const newHeader = document.createElement('h1'); 
+const newHeader = document.createElement("h1");
 
-newHeader.id = 'victory'; 
+newHeader.id = "victory";
 
-newHeader.textContent = 'Kannikar is the champion'; 
+newHeader.textContent = "Kannii is the champion";
 
-document.body.append(newHeader); 
+document.body.append(newHeader);
